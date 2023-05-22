af5a697feb4231cf117030c2d4d23e311cfb4067
bakhacode <bakhamitai@naver.com>
2023-05-23 01:09:54 +0900

    chore: 테일윈드 환경 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        display: "24px",
+        "title-md": "16px",
+        "title-sm": "14px",
+        "title-xs": "12px",
+        "body-md": "16px",
+        "body-sm": "14px",
+        "body-xs": "12px",
+      },
+      fontWeight: {
+        "title-sm": "700",
+      },
+      lineHeight: {
+        "title-sm": "17px",
+      },
+    },
   },
   plugins: [],
 };
